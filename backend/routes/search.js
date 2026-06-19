0cf7cada46bd5de2a27f16287377ed2e5cc25225
Neb465 <benito.f.nie@gmail.com>
2026-06-18 20:56:47 -0400

    request are working

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,26 +1,38 @@
 import express from "express";
+import got from "got";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
 	try {
-		const response = await fetch(
+		//TODO: Response is taking way too long to generate
+		const response = await got.post(
 			"https://api.openrouteservice.org/v2/directions/foot-walking",
 			{
-				method: "POST",
 				headers: {
-					"Authorization": process.env.ors_key,
-					"Content-Type": "application/json"
+					Authorization: process.env.ors_key,
+					"Content-Type": "application/json",
 				},
-				body: JSON.stringify({
-					"coordinates": req.query.coords //figure out how this works later
-				})
-			}
+				json: {
+					coordinates: [
+						[8.681495, 49.41461],
+						[8.686507, 49.41943],
+						[8.687872, 49.420318],
+					],
+				},
+			},
 		);
 
-		const result = await response.json();
+		const result = response.body;
+
+		// const response = await fetch(
+		// 	"https://jsonplaceholder.typicode.com/todos/1",
+		// );
+
+		// const result = await response.json();
+
 		res.json(result);
-	} catch (e){
+	} catch (e) {
 		res.status(500);
 	}
 });
