d0cde109cf976896236d19a9eff11c835568b30f
Neb465 <benito.f.nie@gmail.com>
2026-06-27 12:30:14 -0400

    Added backend src folder + developmental cors

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,22 @@
 import express from "express";
+
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
-import mapAPI from "./routes/mapAPI.js";
-import user from "./routes/user.js";
-import auth from "./routes/auth.js";
+import mapAPI from "./src/routes/mapAPI.js";
+import user from "./src/routes/user.js";
+import auth from "./src/routes/auth.js";
 
-import errorHandler from "./middleware/errorHandler.js";
-import notFound from "./middleware/notFound.js";
+import errorHandler from "./src/middleware/errorHandler.js";
+import notFound from "./src/middleware/notFound.js";
 
 const app = express();
-const port = process.env.PORT || 6767;
+const port = process.env.PORT || 8000;
 
 //Middleware
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors()); //change later to only allow frontend to make requests
 
 //Routes
 app.use("/api/maps", mapAPI);
