9911611c4a04763908e8b29ea8401f26b9805ea5
Neb465 <benito.f.nie@gmail.com>
2026-06-29 11:23:29 -0400

    Rate limit

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import express from "express";
 
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import { rateLimit } from "express-rate-limit";
 
 import mapAPI from "./src/routes/mapAPI.js";
 import user from "./src/routes/user.js";
@@ -10,18 +9,10 @@ import auth from "./src/routes/auth.js";
 
 import errorHandler from "./src/middleware/error-handler.js";
 import notFound from "./src/middleware/not-found.js";
+import { limiter } from "./src/middleware/rate-limiter.js";
 
 const app = express();
 const port = process.env.PORT || 8000;
-
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-	// store: ... , // Redis, Memcached, etc. See below.
-})
 
 //Middleware
 app.use(express.json());
@@ -31,8 +22,8 @@ app.use()
 
 //Routes
 app.use("/api/maps", mapAPI);
-app.use("/api/users", user);
-app.use("/api/auth", auth);
+app.use("/api/users", limiter, user);
+app.use("/api/auth", limiter, auth);
 
 //Error Handler
 app.use(notFound);
