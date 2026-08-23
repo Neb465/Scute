818e4b833dd67919beb30906ad3ae8d4865c75ec
Neb465 <benito.f.nie@gmail.com>
2026-08-23 10:01:57 -0400

    Fix env variables

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(cors({
-	origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+	origin: process.env.FRONTEND_URL || "http://localhost:5173",
 	credentials: true
 }));
 app.use(identifyUser);
